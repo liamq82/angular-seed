f32d2204aae6573c762ff2ddf15e2b3748f60525
liamq82 <liamq82@gmail.com>
2014-06-30 17:03:37 +0100

    started login page
    started html, controller route and unit tests for the login page.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,19 +2,35 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+describe('controllers', function() {
+    beforeEach(module('myApp.controllers'));
+
+    it('should create login model with username and password', inject(function($controller) {
+        var scope = {},
+            LoginController = $controller('LoginController', {
+                $scope: scope
+            });
+        expect(LoginController).toBeDefined();
+        expect(scope.login).toBeDefined();
+        expect(scope.login.username).toEqual('enter username');
+        expect(scope.login.password).toEqual('enter password');
+        
+    }));
 
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('MyCtrl1', { $scope: {} });
-    expect(myCtrl1).toBeDefined();
-  }));
+    it('should ....', inject(function($controller) {
+        //spec body
+        var myCtrl1 = $controller('MyCtrl1', {
+            $scope: {}
+        });
+        expect(myCtrl1).toBeDefined();
+    }));
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('MyCtrl2', { $scope: {} });
-    expect(myCtrl2).toBeDefined();
-  }));
+    it('should ....', inject(function($controller) {
+        //spec body
+        var myCtrl2 = $controller('MyCtrl2', {
+            $scope: {}
+        });
+        expect(myCtrl2).toBeDefined();
+    }));
 });
