665b3b9211ec0a2486236a92b90f234cffd66e2c
liamq82 <liamq82@gmail.com>
2014-07-01 13:34:23 +0100

    User http service
    Created user http service and login button click handler to execute REST
    call.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,12 +8,17 @@ angular.module('myApp.controllers', [])
 
         }
     ])
-    .controller('LoginController', ['$scope',
-        function($scope) {
+    .controller('LoginController', ['$scope', 'User',
+        function($scope, User) {
             $scope.login = {
                 'username': 'enter username',
                 'password': 'enter password'
             };
+
+            $scope.login = function() {
+                $scope.user = User.get();
+            }
+
         }
     ])
     .controller('MyCtrl2', ['$scope',
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,11 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+var myAppServices = angular.module('myApp.services', ['ngResource']);
+
+myAppServices.value('version', '0.1');
+
+myAppServices.factory('User', ['$resource',
+  function($resource){
+    return $resource('/user/:userName', {userName:'@username'}); 
+  }]);
