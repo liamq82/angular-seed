779a35df323580b0e20101858ae68ea2dd51b18a
liamq82 <liamq82@gmail.com>
2014-07-01 12:18:06 +0100

    Login page and integration test
    Created input fields for login credentials, bound to login controller
    and integration tests for Login page.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,39 +4,55 @@
 
 describe('my app', function() {
 
-  browser.get('index.html');
+    describe('login view', function() {
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
+        beforeEach(function() {
+            browser.get('index.html#/login');
+        });
+
+        it('should render login page when user navigates to /login', function() {
+            expect(browser.getLocationAbsUrl()).toMatch("/login");
+            expect(element.all(by.css('[ng-view] h1')).first().getText()).
+            toMatch(/Login/);
+        });
+
+        it('should render a username and password input field', function() {
+            var usernameInput = element(by.model('login.username'));
+            var passwordInput = element(by.model('login.password'));
+            expect(usernameInput.getAttribute('placeholder')).toBe('enter username');
+            expect(passwordInput.getAttribute('placeholder')).toBe('enter password');
+        });
 
 
-  describe('view1', function() {
 
-    beforeEach(function() {
-      browser.get('index.html#/view1');
+    });
+
+    describe('view1', function() {
+
+        beforeEach(function() {
+            browser.get('index.html#/view1');
+        });
+
+
+        it('should render view1 when user navigates to /view1', function() {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+            toMatch(/partial for view 1/);
+        });
+
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    describe('view2', function() {
+
+        beforeEach(function() {
+            browser.get('index.html#/view2');
+        });
+
+
+        it('should render view2 when user navigates to /view2', function() {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+            toMatch(/partial for view 2/);
+        });
+
     });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
