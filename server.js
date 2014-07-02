fea54935c87123fcde2ce3bc3aa4e2e33531c392
liamq82 <liamq82@gmail.com>
2014-07-02 20:14:40 +0100

    Added database and server routing
    Added MongoDB and Mongoose to connect. Added server logic to read/write
    users to database.

--- server.js
+++ server.js
@@ -1,49 +1,52 @@
 var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/users');
 app.use(bodyParser());
 
 var port = process.env.PORT || 8080; // set our port
 
+var User = require('./app/models/user');
+
 var router = express.Router(); // get an instance of the express Router
 
-app.post('/user', function(req, res) {
-        // `req.user` contains the authenticated user.
-        console.log(req.body);
-        //res.redirect('http://localhost:8000/app/#/inventory');
-        res.send({
-            message: 'Login reuqest recieved!',
-            user: req.body.username,
-            password: req.body.password
+router.route('/user').post(function(req, res) {
+
+    var user = new User(); // create a new instance of the Bear model
+    user.username = req.body.username; // set the bears name (comes from the request)
+    user.password = req.body.password;
+
+    // save the bear and check for errors
+    user.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({
+            message: 'User created!'
         });
+    });
 
 });
 
 router.route('/user').get(function(req, res) {
-    res.json([{
-        'username': 'liam',
-        'password': 'hello'
-    }, {
-        'username': 'john',
-        'password': 'goodbye'
-    }, {
-        'username': 'jamie',
-        'password': 'gone'
-    }, {
-        'username': 'gar',
-        'password': 'done'
-    }]);
+    User.find(function(err, users) {
+        if (err)
+            res.send(err);
+
+        res.json(users);
+    });
 });
 
-router.route('/user/:user_id').get(function(req, res) {
-    var id = req.params.user_id;
-    var user = {
-        'id': id,
-        'username': 'test user',
-        'password': 'test password'
-    };
-    res.json(user);
+router.route('/user').delete(function(req, res) {
+    User.remove({
+        _id: req.params.user_id
+    }, function(err, user) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'User successfully deleted' });
+    });
 });
 
 app.use(router);
